95ea0910bd6de0319c5036d815deab9d54e49d9c
Anna Shafeeva <dzenborg@gmail.com>
2022-07-28 11:58:45 +0300

    Добавлены товары по акции

--- src/Main.java
+++ src/Main.java
@@ -72,5 +72,77 @@ public class Main {
         }
 
         System.out.println("Итого: " + ollSum);
+
+        String[] saleProducts = {
+                "Йогурт",
+                "Печенье",
+                "Макароны"
+        };
+
+        int[] salePrices = {
+                30,
+                50,
+                100
+        };
+
+        int[] saleNumb = new int[salePrices.length];
+
+        System.out.println(System.lineSeparator() + "Список товаров по акции 2=3: ");
+        for (int j = 0; j < saleProducts.length; j++) {
+            System.out.println((j + 1) + ". " + saleProducts[j] + " " + salePrices[j] + " руб/шт");
+        }
+
+        int saleOllSum = 0;
+        int saleNumProduct;
+        int saleAmount;
+        double sumPoz;
+
+
+        while (true) {
+            System.out.println(System.lineSeparator() + "Выберите товар и количество или нажмите end");
+            String input = scanner.nextLine();//1 2
+            if (input.equals("end")) {
+                break;
+            }
+            String[] parts = input.split(" ");
+
+            if (parts.length != 2) {
+                System.out.println("Некорректный ввод! Нужно ввести два числа!");
+                continue;
+            }
+
+            try {
+                saleNumProduct = Integer.parseInt(parts[0]) - 1;
+                saleAmount = Integer.parseInt(parts[1]);
+            } catch (NumberFormatException e) {
+                System.out.println("Ошибка ввода! Нужно вводить только числа, а не текст!");
+                continue;
+            }
+
+            if (saleNumProduct < 0 || saleNumProduct >= saleProducts.length) {
+                System.out.println("Некорректный ввод позиции! Нужно выбрать номер позиции из списка!");
+                continue;
+            }
+
+            saleNumb[saleNumProduct] += saleAmount;//сумма штук введенного
+        }
+
+        System.out.println("Ваша корзина c товарами по акции: ");
+
+        for (int i = 0; i < saleNumb.length; i++) {
+            if (saleNumb[i] != 0) {
+                sumPoz = (3 * (int) (saleNumb[i] / 3) * salePrices[i]) / 1.5 +
+                        (saleNumb[i] - 3 * (int) (saleNumb[i] / 3)) * salePrices[i];
+
+                System.out.println(saleProducts[i] + " " + saleNumb[i] + " шт " +
+                        salePrices[i] + " руб/шт " + sumPoz + " в сумме");
+
+                saleOllSum += sumPoz;
+            }
+        }
+
+        System.out.println("Итого по акции: " + saleOllSum);
+
+        System.out.println(System.lineSeparator() + "Итого за все: " + (ollSum + saleOllSum));
     }
 }
